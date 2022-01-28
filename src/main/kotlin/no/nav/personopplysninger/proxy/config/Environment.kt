79af2bc1fbb0889072c38e8761b307e56e59b2a1
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2022-01-28 12:17:28 +0100

    Midlertidig hardkodet corsAllowedOrigins

--- src/main/kotlin/no/nav/personopplysninger/proxy/config/Environment.kt
+++ src/main/kotlin/no/nav/personopplysninger/proxy/config/Environment.kt
@@ -1,9 +1,8 @@
 package no.nav.personopplysninger.proxy.config
 
-import no.nav.personbruker.dittnav.common.util.config.StringEnvVar.getEnvVar
-
 data class Environment(
-    val corsAllowedOrigins: String = getEnvVar("CORS_ALLOWED_ORIGINS")
+    //val corsAllowedOrigins: String = getEnvVar("CORS_ALLOWED_ORIGINS")
+    val corsAllowedOrigins: String = "*"
 
     // InfluxDB
     /*
