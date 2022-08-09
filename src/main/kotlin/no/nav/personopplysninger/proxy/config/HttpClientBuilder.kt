ead0e11a35efdbf306b6762a276326b72a12764d
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2022-08-09 14:52:22 +0200

    Fikset feil i HttpClient

--- src/main/kotlin/no/nav/personopplysninger/proxy/config/HttpClientBuilder.kt
+++ src/main/kotlin/no/nav/personopplysninger/proxy/config/HttpClientBuilder.kt
@@ -4,13 +4,14 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.apache.Apache
 import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
 
 object HttpClientBuilder {
 
     fun build(): HttpClient {
         return HttpClient(Apache) {
             install(ContentNegotiation) {
-                jsonConfig()
+                json(jsonConfig())
             }
             install(HttpTimeout)
             expectSuccess = false
