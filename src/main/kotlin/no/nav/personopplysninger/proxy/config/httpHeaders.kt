664c31545fa63a3f0501dbbd2e2d8034eb173c77
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2022-11-22 09:54:14 +0100

    Fjernet ubrukt kode

--- src/main/kotlin/no/nav/personopplysninger/proxy/config/httpHeaders.kt
+++ src/main/kotlin/no/nav/personopplysninger/proxy/config/httpHeaders.kt
@@ -1,14 +1,10 @@
 package no.nav.personopplysninger.proxy.config
 
-import io.ktor.http.*
+import io.ktor.http.HttpHeaders
 
 val HttpHeaders.NavCallId: String
     get() = "Nav-Call-Id"
 val HttpHeaders.NavConsumerId: String
     get() = "Nav-Consumer-Id"
-val HttpHeaders.NavConsumerToken: String
-    get() = "Nav-Consumer-Token"
 val HttpHeaders.NavPersonident: String
     get() = "Nav-Personident"
-val HttpHeaders.Enhetsnr: String
-    get() = "enhetsnr"
--- src/main/kotlin/no/nav/personopplysninger/proxy/sts/TokenDto.kt
+++
@@ -1,8 +0,0 @@
-package no.nav.personopplysninger.proxy.sts
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class TokenDto (
-    val access_token: String = ""
-)
